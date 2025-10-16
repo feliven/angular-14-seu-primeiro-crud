3e7197b74a27e84662719f9c73c86333f55e8423
FVS <feliven@gmail.com>
2025-10-16 14:18:30 -0300

    criado listar-pensamento

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,11 @@ import { RouterModule } from '@angular/router';
 import { Cabecalho } from './components/cabecalho/cabecalho';
 import { Rodape } from './components/rodape/rodape';
 import { CriarPensamento } from './components/pensamentos/criar-pensamento/criar-pensamento';
+import { ListarPensamento } from './components/pensamentos/listar-pensamento/listar-pensamento';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, Cabecalho, Rodape, CriarPensamento],
+  imports: [RouterModule, Cabecalho, Rodape, CriarPensamento, ListarPensamento],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  imports: [],
+  templateUrl: './listar-pensamento.html',
+  styleUrl: './listar-pensamento.css'
+})
+export class ListarPensamento {
+
+}
