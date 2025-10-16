1f5127f59d94cf56eb9e2fc6980dd70792b29382
FVS <feliven@gmail.com>
2025-10-16 14:22:26 -0300

    criadas Routes para criarPensamento e listarPensamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CriarPensamento } from './components/pensamentos/criar-pensamento/criar-pensamento';
+import { ListarPensamento } from './components/pensamentos/listar-pensamento/listar-pensamento';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'listarPensamento', pathMatch: 'full' },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamento,
+  },
+
+  {
+    path: 'listarPensamento',
+    component: ListarPensamento,
+  },
+];
